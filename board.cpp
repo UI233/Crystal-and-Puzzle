b8765cd783b2102eb3ed816c88116c86429eb702
UI233 <34206293+UI233@users.noreply.github.com>
2018-06-20 22:48:46 +0800

    Add Board class declaration
    用Board类来作为显示高分榜和帮助文档的基类

---
+++ board.cpp
@@ -0,0 +1,14 @@
+#include "board.h"
+#include "ui_board.h"
+
+Board::Board(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Board)
+{
+    ui->setupUi(this);
+}
+
+Board::~Board()
+{
+    delete ui;
+}
