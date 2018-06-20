7adb12ec33bbaba9497c2f0b38277d804618baed
UI233 <34206293+UI233@users.noreply.github.com>
2018-06-21 01:12:09 +0800

    Audio supported!!!
    加了首测试用的bgm...debug的时候可以以此消遣？

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMediaPlayer>
+#include <QUrl>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+    QMediaPlayer *musicPlayer = new QMediaPlayer(&a);
+    musicPlayer -> setMedia(QUrl("qrc:/Resources/bgm.mp3"));
+    musicPlayer -> setVolume(100);
+    musicPlayer -> play();
     return a.exec();
 }
