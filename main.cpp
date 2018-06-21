206b8f4a4373f3089e06fe8e2035b7a6c3f82f51
UI233 <34206293+UI233@users.noreply.github.com>
2018-06-21 23:42:53 +0800

    Bgm Volume changed

--- main.cpp
+++ main.cpp
@@ -7,14 +7,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QString path=QDir::currentPath();
-    qDebug()<<"the path is  "<<path<<endl;
     MainWindow w;
     w.show();
 
     QMediaPlayer *musicPlayer = new QMediaPlayer(&a);
     musicPlayer -> setMedia(QUrl("qrc:/Resources/bgm.mp3"));
-    musicPlayer -> setVolume(100);
+    musicPlayer -> setVolume(80);
     musicPlayer -> play();
 
     return a.exec();
